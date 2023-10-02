a7e53439c70a3dab93795896d2b8753e39c204c5
Jorge Marcos Vives <jorgemarcosvives@gmail.com>
2023-10-02 19:56:47 +0200

    he actualizado el List.h

--- List.h
+++ List.h
@@ -7,7 +7,7 @@ using namespace std;
 template <typename T> 
 class List {
     public:
-	void instert(int pos, T e);
+	void insert(int pos, T e);
 	void append(T e);
 	void prepend(T e);
 	T remove(int pos);
